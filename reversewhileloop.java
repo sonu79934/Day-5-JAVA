d1f52fc45e91b719a849df3b8d002fd6f3b7438d
sonu79934 <yogasritha8@gmail.com>
2024-12-06 10:57:25 +0530

    Add files via upload

---
+++ reversewhileloop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class reversewhileloop{
+    public static void main(String args[]){
+        Scanner sc=new Scanner( System.in);
+        int n=sc.nextInt();
+         int rev=0;
+          while(n>0)  
+        {
+        int dig=n%10;
+        rev=rev*10+dig;
+        n/=10;
+        }                                                                                                                                                                              
+        System.out.println(rev);
+    }
+}
