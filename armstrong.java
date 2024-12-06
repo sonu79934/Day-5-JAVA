ccde63aadf39481145e644e4841aa3cece2de67b
sonu79934 <yogasritha8@gmail.com>
2024-12-06 10:57:11 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class armstrong{
+    public static void main(String args[]){
+        Scanner sc=new Scanner( System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+          int n=sc.nextInt();
+          int temp=n;
+          int rev=0;
+          while(n>0)  
+        {
+        int dig=n%10;
+        rev=rev+dig*dig*dig;
+        n/=10;
+        }
+        System.out.println(rev);
+        if(temp==rev) System.out.println("Armstorng Number");
+        else  System.out.println(" not Armstrong Number");
+        }
+    }
+}
