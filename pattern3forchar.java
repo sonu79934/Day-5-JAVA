c86ab4a1abd4acbe35d7ebb8865a1171aec55db0
sonu79934 <yogasritha8@gmail.com>
2024-12-06 15:10:34 +0530

    Add files via upload

---
+++ pattern3forchar.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pattern3forchar{
+    public static void main(String args[]){
+        Scanner sc=new Scanner( System.in);
+        int n=sc.nextInt(); 
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=n;j>=i;j--)
+            {
+            System.out.print((char)(64+j)+" ");
+            }
+        System.out.println();
+        }
+    }
+}
