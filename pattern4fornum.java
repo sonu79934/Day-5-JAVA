52627d168e342a7a84fde26b8aef0f738174953a
sonu79934 <yogasritha8@gmail.com>
2024-12-07 09:31:08 +0530

    Add files via upload

---
+++ pattern4fornum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class pattern4fornum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner( System.in);
+        int n=sc.nextInt(); 
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+            System.out.print("*");
+            }
+        System.out.println();
+        }
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=n-1;j>=1;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
